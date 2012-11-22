93684bb27569dcbdc2bf53127e65bf2ec0026670
Manuel Velasco <manuel.velasco.b@gmail.com>
2012-11-21 21:58:17 -0500

    primera edicion
    proyecto hecho en clase app dsitrib

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Losestudiantes::Application.config.secret_token = '1c830258d7b3580de8d00b3e7557582cebdf32fdf21ececf86cf20bb98766ea81ed5ff824929be27f40e6092c959c8a1401c5efe84b9bff4bd73f09fd91b7d67'
